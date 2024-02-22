8a96f22ebbed4247388f0da39b1099787873c538
thesanju <sanjayjr1official@gmail.com>
2024-02-22 19:44:09 +0530

    changed the style of PDFviewer and added chatbox

--- src/components/Pdfview.jsx
+++ src/components/Pdfview.jsx
@@ -7,26 +7,27 @@ function Pdfview() {
   const { PdfUrl } = useParams();
 
   return (
-    <>
-    <div style={{ textAlign: 'center' , marginTop: '20px' }}>
-      <h2 style={{
-        fontFamily: 'Roboto, sans-serif',
-        fontWeight: 750,
-        color: '#1F2544'
-      }}>all the best bro 🥲</h2>
-      <iframe
-        title="PDF Preview"
-        src={decodeURIComponent(PdfUrl)}
-        width="800"
-        height="900"
-        style={{ border: '1px solid #ccc', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}
-        allow="autoplay"
-      ></iframe>
+    <div style={{ display: 'flex' }}>
+      <div style={{ flex: 1, textAlign: 'center', marginTop: '20px' }}>
+        <h2 style={{
+          fontFamily: 'Roboto, sans-serif',
+          fontWeight: 750,
+          color: '#1F2544'
+        }}>all the best bro 🥲</h2>
+        <iframe
+          title="PDF Preview"
+          src={decodeURIComponent(PdfUrl)}
+          width="800"
+          height="900"
+          style={{ border: '1px solid #ccc', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}
+          allow="autoplay"
+        ></iframe>
+      </div>
+
+      <div style={{ flex: 1 }}>
+        <Chat></Chat>
+      </div>
     </div>
-    <div>
-      <Chat></Chat>
-    </div>
-    </>
   );
 }
 
